8b2f73c0a9c2adf89b74e70cc19a386c13c9fefa
HAndyUA <andrew.hnatyuk@icloud.com>
2016-09-08 06:17:09 +0300

    8
    первая протестированая

--- main/main.ino
+++ main/main.ino
@@ -5,6 +5,12 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2); // (RS, E, DB4, DB5, DB6, DB7)
 // BT - Bottle Layer
 
 const byte cVers = 7;
+byte cNum0 = 20; //число периодов несущей 38кГц в посылаемом пакете
+byte cImpulse0 = 9;  // длительность полупериода
+byte cNum1 = 20;
+byte cImpulse1 = 10;
+byte cNum2 = 20;
+byte cImpulse2 = 10;
 const byte cDelay = 55;
 const byte cExistEnough = 155;
 const byte cCheckOutPeriod = 33;
@@ -23,10 +29,8 @@ const byte cLED2 = 13;
 const byte c0L = 0;
 const byte c1L = 1;
 const byte c2L = 2;
+const byte cFake = 3;
 const byte cVolumeDetermination = 3;
-
-byte cNum = 20; //число периодов несущей 38кГц в посылаемом пакете
-byte cImpulse = 10;  // длительность полупериода
 
 long vProcessed[3];
 boolean vBLExists[3];
@@ -34,10 +38,9 @@ boolean vBLExists[3];
 long vBLExisting[3];
 byte vBLAbsenting[3];
 byte vInspectingBL;
-byte vCurrBL;
 byte vPegasStatus = cVolumeDetermination;
-long vExistEnough = cExistEnough;
-byte vCheckOutPeriod = cCheckOutPeriod;
+//long vExistEnough = cExistEnough;
+//byte vCheckOutPeriod = cCheckOutPeriod;
 
 void setup()
 {
@@ -54,72 +57,57 @@ void setup()
   pinMode(cLED2, OUTPUT);
 }
 
-void loop() {
-  //Отключаем обработку не задействованых датчиков
-  if (vPegasStatus > c1L) {
-    processBL(c2L, cSensor2, cLED2);
-  }
-  if (vPegasStatus > c0L) {
-    processBL(c1L, cSensor1, cLED1);
-  }
-  processBL(c0L, cSensor0, cLED0);
+void loop() {    
+  processBL(c2L, cSensor2, cLED2, cNum2, cImpulse2);
+  processBL(c1L, cSensor1, cLED1, cNum1, cImpulse1);
+  processBL(c0L, cSensor0, cLED0, cNum0, cImpulse0);
 }
 
-void processBL(byte pBottLayer, byte pSensor, byte pLED) {
-  vCurrBL = pBottLayer;
+void processBL(byte pBottLayer, byte pSensor, byte pLED, byte pNum, byte pImpulse) {
+  maintainBL(pBottLayer, pSensor, pLED, pNum, pImpulse);
+  
   if (vPegasStatus == cVolumeDetermination) {
-    maintainBL(pBottLayer, pSensor, pLED);
-
-    if (vBLExists[vInspectingBL]) {
-      byte tParentLayerExisting = 0;
-      if (vInspectingBL < c2L) {
-        tParentLayerExisting = vBLExisting[vInspectingBL + 1];
-      }
-      if (vBLExisting[vInspectingBL] - tParentLayerExisting > cCheckOutPeriod) {
-        vPegasStatus = vInspectingBL;
-        lcd.setCursor(0, 1);
-        lcd.print("            ");
-        vExistEnough = cExistEnough * (vPegasStatus + 1);
-        vCheckOutPeriod = cCheckOutPeriod * (vPegasStatus + 1);
-      }
+    if (vBLExisting[vInspectingBL] == cExistEnough) {
+      vPegasStatus = vInspectingBL;
+    } else {
+      if (vBLAbsenting[vInspectingBL] == cCheckOutPeriod) {bottProcessed(cFake);}
     }
-
   } else {
-
-    maintainBL(vPegasStatus, pSensor, pLED);
-    if (vBLAbsenting[vPegasStatus] == vCheckOutPeriod) {
-      if (vBLExisting[vInspectingBL] == vExistEnough - vCheckOutPeriod) {
-        bottProcessed(true);
+    if (vBLAbsenting[vPegasStatus] == cCheckOutPeriod) {
+      //c2L достаточно значимый к общему обьему и c1L подверждает консистентность значения, а не является просто большой помехой
+      if ((vBLExisting[c2L] / (vBLExisting[c0L] + vBLExisting[c1L] +0.1) > 0.3) && (vBLExisting[c2L] / (vBLExisting[c1L] +0.1)> 0.2)) { 
+        bottProcessed(c2L);
       } else {
-        bottProcessed(false);
+        if ( vBLExisting[c1L] / (vBLExisting[c0L] +0.1) > 0.4) {
+          bottProcessed(c1L); 
+        } else {
+          bottProcessed(c0L);  
+        }
       }
     }
   }
 }
-
-boolean maintainBL(byte pBL, byte pSensor, byte pLED) {
+boolean maintainBL(byte pBL, byte pSensor, byte pLED, byte pNum, byte pImpulse) {
   vInspectingBL = pBL;
   delay(cDelay);
-  for (int i = 0; i < cNum; i++) //цикл несущей
+  for (int i = 0; i < pNum; i++) //цикл несущей
   {
     digitalWrite(pLED, HIGH); //зажигаем ик-диод
-    delayMicroseconds(cImpulse); //положительный полупериод
+    delayMicroseconds(pImpulse); //положительный полупериод
     digitalWrite(pLED, LOW); //гасим ик-диод
-    delayMicroseconds(cImpulse - 1); //выравниваем скважность
+    delayMicroseconds(pImpulse - 1); //выравниваем скважность
   }
   vBLExists[vInspectingBL] = !digitalRead(pSensor);
   changeCounters();
 }
 
-void bottProcessed(boolean Appropriate) {
-  if (Appropriate) {
-    vProcessed[vInspectingBL]++;
-  }
+void bottProcessed(byte pBL) {
+  if (pBL != cFake) {vProcessed[pBL]++;}
   vPegasStatus = cVolumeDetermination;
   lcd.setCursor(0, 0);
   lcd.print("            ");
-  lcd.setCursor(12, 0);
-  lcd.print(vProcessed[0] + vProcessed[1] * 2 + vProcessed[2] * 4);
+  lcd.setCursor(13, 0);
+  lcd.print(vProcessed[c0L] + vProcessed[c1L] * 2 + vProcessed[c2L] * 4);
   lcd.setCursor(0, 1);
   lcd.print("             ");
 
@@ -130,44 +118,37 @@ void bottProcessed(boolean Appropriate) {
 
 void resetBottLayer(byte pLayer) {
   vBLExists[pLayer] = false;
-
+  lcd.setCursor(pLayer * 4 +2, 0);
+  lcd.print(vProcessed[pLayer]);
   vBLExisting[pLayer] = 0;
   vBLAbsenting[pLayer] = 0;
-
-  lcd.setCursor(pLayer * 4 +1, 0);
-  lcd.print(vProcessed[pLayer]);
 }
 
 void changeCounters() {
-  //lcd.setCursor(vInspectingBL * 3, 0);
-  //lcd.print("  ");
-  lcd.setCursor(vCurrBL * 4, 0);
-  lcd.print(" ");
-  lcd.setCursor(vCurrBL * 4, 1);
-  lcd.print(" ");
+
+  lcd.setCursor(vInspectingBL * 4, 0);
+  lcd.print("  ");
+  lcd.setCursor(vInspectingBL * 4, 1);
+  lcd.print("     ");
   
   if (vBLExists[vInspectingBL]) {
-    lcd.setCursor(vCurrBL * 4, 0);
-    lcd.print("\xFF");
-    lcd.setCursor(vCurrBL * 4, 1);
-    lcd.print("\xFF");
-    if (vBLExisting[vInspectingBL] < vExistEnough) {
-      vBLExisting[vInspectingBL]++;
-    }
     if (vBLAbsenting[vInspectingBL] > 0) {
       vBLAbsenting[vInspectingBL]--;
-    }
+      }
+    vBLExisting[vInspectingBL]++;
+    lcd.setCursor(vInspectingBL * 4, 1);
+    //lcd.print("\xFF");
+    lcd.print(vBLExisting[vInspectingBL]);
+    
   } else {
-    if (vBLAbsenting[vInspectingBL] < vCheckOutPeriod) {
-      vBLAbsenting[vInspectingBL]++;
-    }
     if (vBLExisting[vInspectingBL] > 0) {
+      vBLAbsenting[vInspectingBL]++;
       vBLExisting[vInspectingBL]--;
-    }
+      lcd.setCursor(vInspectingBL * 4, 0);
+      lcd.print(vBLAbsenting[vInspectingBL]);
+    } 
   }
-
-  lcd.setCursor(vInspectingBL * 4 +1, 1);
-  lcd.print("   ");
-  lcd.setCursor(vInspectingBL * 4 +1, 1);
-  lcd.print(vBLExisting[vInspectingBL]);
 }
+
+
+
